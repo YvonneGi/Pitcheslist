779a2df34ed08a291a3fce8c5cd23d33bf5681ee
YvonneGi <giryvonne@gmail.com>
2019-09-20 17:33:02 +0200

    add pitch function

--- app/main/views.py
+++ app/main/views.py
@@ -15,6 +15,47 @@ def index():
     title = 'Home - Welcome to The best Pitches Website'
     return render_template('index.html', title = title)
 
+@main.route('/category/<int:id>')
+def category(id):
+
+    '''
+    View Category page function that returns the page where you add pitch
+    '''
+    category = get_category(id)
+    cat_name = f'{category.cat_name}'
+    pitch = Pitch.get_pitch(category.id)
+   
+
+    return render_template('categories.html',cat_name = cat_name,category = category,pitch = pitch)
+
+@main.route('/category/pitch/new/<int:id>', methods = ['GET','POST'])
+@login_required
+def new_pitch(id):
+    form = PitchForm()
+    category = get_category(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        pitch = form.pitch.data
+        
+           # Updated pitch instance
+        new_pitch = Pitch(category_id=category.id,cat_name=cat_name,category_pitch=review,user=current_user)
+
+        # save pitch method
+        new_pitch.save_pitch()
+        return redirect(url_for('.category',id = category.id ))
+
+    cat_name = f'{category.cat_name} pitch'
+    return render_template('new_pitch.html',cat_name = cat_name, pitch_form=form, category=category)
+
+@main.route('/pitch/<int:id>')
+def single_pitch(id):
+    pitch=Pitch.query.get(id)
+    if pitch is None:
+        abort(404)
+    format_pitch = markdown2.markdown(pitch.category_pitch,extras=["code-friendly", "fenced-code-blocks"])
+    return render_template('pitch.html',pitch = pitch,format_pitch=format_pitch)
+
 @main.route('/user/<uname>')
 def profile(uname):
     user = User.query.filter_by(username = uname).first()
